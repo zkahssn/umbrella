aac429330c6ed89ecc23c6b924194aff0f77bb23
zakia <zakia@Zakias-MBP.localdomain>
2019-05-08 15:47:49 +0100

    Processing responses

--- js/index.js
+++ js/index.js
@@ -1,21 +1,34 @@
+var responseObject;
+
+function callback(response){
+    console.log("This is the callback " + response);
+    
+}
+
 function httpGet(theUrl)
 {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open( "GET", theUrl, true); 
+    xmlHttp.responseType = "json"
     xmlHttp.onload = function (e){
     if(xmlHttp.readyState === 4){
         if(xmlHttp.status === 200){
-            console.log(xmlHttp.responseText);
+            callback(xmlHttp.response);
+            responseObject = xmlHttp.response;
+            console.log(xmlHttp.response);
         }
         else{
-            console.error(xmlHttp.statusText);
+            var weather = Http.statusText;
+            //return weather;
         }
     }
     }
     xmlHttp.onerror = function(e){
-        console.error(xmlHttp.statusText)
+        return xmlHttp.statusText
     
     }
     xmlHttp.send( null );
-    console.log("This is the response " + xmlHttp.responseText);
 }
+
+
+
